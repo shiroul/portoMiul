09abd4e87ae54082cd4582941dfe15adae9395fe
shiroul <mjamiulakbar@gmail.com>
2024-10-09 12:49:29 +0700

    first commit in aboutme branch

---
+++ logic.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    console.log(scrollY)
+}

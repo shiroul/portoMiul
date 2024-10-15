469f07cf18bfda6074247b603fcb774d6b6dd826
shiroul <mjamiulakbar@gmail.com>
2024-10-15 14:34:45 +0700

    add new content inside about me

--- logic.js
+++ logic.js
@@ -2,8 +2,8 @@ var header = document.getElementById("header");
 var sticky = header.offsetTop;
 
 window.addEventListener("scroll", function() {
-    console.log('banana')
     if(scrollY > sticky){
+
         header.classList.add('stickyHeader')
         return
     }
